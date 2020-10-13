69d18853029c1eaf39831b0f0ddfd7bc973d7610
sifat007 <sifat27.ht@gmail.com>
2020-10-13 18:39:12 +0600

    create add pAssword and show all password

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,12 @@ router.get('/passwordCategory', function(req, res, next) {
 router.get('/add-new-category', function(req, res, next) {
   res.render('addNewCategory', { title: 'Password Management System', msg:'' });
 });
+router.get('/add-new-password', function(req, res, next) {
+  res.render('add-new-password', { title: 'Password Management System', msg:'' });
+});
+router.get('/view-all-password', function(req, res, next) {
+  res.render('view-all-password', { title: 'Password Management System', msg:'' });
+});
 
 
 
