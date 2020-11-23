44c2112b3ea034a06ba9d68a2a5dde446138d297
sifat007 <sifat27.ht@gmail.com>
2020-11-23 17:58:20 +0600

    Api error handeling for validation mongojs

--- api/add-category.js
+++ api/add-category.js
@@ -26,11 +26,22 @@ router.post("/add-category",function(req,res,next){
 
     var passCategory=req.body.pass_cat;
     var passCatDetail=new passCatModel({passord_category:passCategory});
-    passCatDetail.save(function(err,doc){
-        if(err)throw err;
-        res.send("success---Nodejs restFull Api POST method Working");
+    // passCatDetail.save(function(err,doc){
+    //     if(err)throw err;
+    //     res.send("success---Nodejs restFull Api POST method Working");
         
-    }) 
+    // }) 
+    passCatDetail.save()
+    .then(doc=>{
+        res.status(201).json({
+            messege:"Category saved successfully",
+            result:doc
+        });
+    })
+    .catch(err=>{
+         res.json(err);
+    });
+
 });
 
 
@@ -43,9 +54,20 @@ router.put("/add-update-category/:id",function(req,res,next){
     passCatModel.findById(id,function(err,data){
 
         data.passord_category=passCategory?passCategory:data.passord_category;
-        data.save(function(){
-            if(err)throw err;
-            res.send("Nodejs restFull Api PUT method Working----Data update successfully");
+        // data.save(function(){
+        //     if(err)throw err;
+        //     res.send("Nodejs restFull Api PUT method Working----Data update successfully");
+        // });
+
+        data.save()
+        .then(doc=>{
+            res.status(201).json({
+                messege:"Category update successfully",
+                result:doc
+            });
+        })
+        .catch(err=>{
+             res.json(err);
         });
         
     })
@@ -77,14 +99,22 @@ router.patch("/update-category/:id",function(req,res,next){
 
 router.delete("/add-delte-category/:id",function(req,res,next){
     var id=req.params.id;
-    passCatModel.findById(id,function(err,data){
+    // passCatModel.findById(id,function(err,data){
+    //     data.delete(function(){
+    //         if(err)throw err;
+    //         res.send("Nodejs restFull Api DELTE method Working----Data DELETE successfully");
+    //     });    
+    // })
 
-       
-        data.delete(function(){
-            if(err)throw err;
-            res.send("Nodejs restFull Api DELTE method Working----Data DELETE successfully");
+    passCatModel.findByIdAndRemove(id)
+    .then(doc=>{
+        res.status(201).json({
+            messege:"Category deleted successfully",
+            result:doc
         });
-        
+    })
+    .catch(err=>{
+         res.json(err);
     })
 });
 
